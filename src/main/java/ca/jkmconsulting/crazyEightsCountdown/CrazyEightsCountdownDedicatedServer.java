3d0acbf0d5ba622f0d77daab6e74136f03c2e2b4
Jayson Mendoza <jayson.k.mendoza@gmail.com>
2022-11-18 14:18:39 -0500

    Added basic springboot async app and tested it runs.

---
+++ src/main/java/ca/jkmconsulting/crazyEightsCountdown/CrazyEightsCountdownDedicatedServer.java
@@ -0,0 +1,29 @@
+package ca.jkmconsulting.crazyEightsCountdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@SpringBootApplication
+@EnableAsync
+public class CrazyEightsCountdownDedicatedServer {
+    public static void main(String[] args) {
+        SpringApplication.run(CrazyEightsCountdownDedicatedServer.class, args);
+    }
+
+    @Bean(name="asyncExecutor")
+    public Executor asyncExecutor()
+    {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(20);
+        executor.setMaxPoolSize(100);
+        executor.setQueueCapacity(1000);
+        executor.setThreadNamePrefix("AsyncThreadQuestGame-");
+        executor.initialize();
+        return executor;
+    }
+}
