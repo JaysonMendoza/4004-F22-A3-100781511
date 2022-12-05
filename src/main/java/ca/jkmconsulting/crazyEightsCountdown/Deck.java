d2e43cacea421cd14513829cf4ad04dfc1bf6935
Jayson Mendoza <jayson.k.mendoza@gmail.com>
2022-12-04 20:25:28 -0500

    Test I1: testDeckBuildingHasAllCards. Includes stacked deck and random deck

---
+++ src/main/java/ca/jkmconsulting/crazyEightsCountdown/Deck.java
@@ -0,0 +1,29 @@
+package ca.jkmconsulting.crazyEightsCountdown;
+
+import java.util.ArrayList;
+
+public class Deck {
+    private ArrayList<Card> drawDeck;
+    private ArrayList<Card> cardsIssued;
+    private ArrayList<Card> discardPile;
+
+    public Deck(ArrayList<Card> fixedOrder) {
+
+    }
+
+    public Deck() {
+
+    }
+
+    private void buildDeck(ArrayList<Card> cardOrder) {
+
+    }
+
+    public int getNumCardsInDeck() {
+        return -1;
+    }
+
+    public Card drawCard() {
+        return null;
+    }
+}
---
+++ src/test/java/ca/jkmconsulting/crazyEightsCountdown/DeckTest.java
@@ -0,0 +1,69 @@
+package ca.jkmconsulting.crazyEightsCountdown;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    public void I1_testDeckBuildingHasAllCards() {
+        //Case 1 : Standard deck
+        ArrayList<Card> allCards = new ArrayList<>(Arrays.asList(Card.values()));
+        ArrayList<Card> test1 = new ArrayList<>(allCards);
+        assertEquals(allCards.size(),test1.size());
+        Deck deck = new Deck();
+        for(int i=0;i<allCards.size();++i) {
+            Card c = deck.drawCard();
+            assertTrue(test1.remove(c));
+        }
+        assertEquals(0,test1.size());
+
+        //Case 2: Deck built with a specific order for testing
+        ArrayList<Card> test2 = new ArrayList<>();
+        test2.add(Card.CLUBS_4);
+        test2.add(Card.DIAMONDS_ACE);
+        test2.add(Card.CLUBS_JACK);
+        test2.add(Card.HEARTS_QUEEN);
+        test2.add(Card.SPADES_KING);
+        test2.add(Card.SPADES_QUEEN);
+        test2.add(Card.HEARTS_2);
+        test2.add(Card.HEARTS_3);
+        test2.add(Card.HEARTS_4);
+        test2.add(Card.HEARTS_5);
+        test2.add(Card.HEARTS_6);
+        test2.add(Card.HEARTS_7);
+        test2.add(Card.HEARTS_8);
+        test2.add(Card.HEARTS_9);
+        test2.add(Card.HEARTS_10);
+        test2.add(Card.HEARTS_JACK);
+        test2.add(Card.HEARTS_KING);
+        test2.add(Card.HEARTS_ACE);
+        int lastIndex = test2.size()-1;
+        allCards.forEach(c ->{
+           if(!test2.contains(c)) {
+               test2.add(c);
+           }
+        });
+        assertEquals(allCards.size(),test2.size());
+        deck = new Deck(test2);
+        for(int i=0;i<allCards.size();++i) {
+            Card c = deck.drawCard();
+            if(i<=lastIndex) {
+                assertEquals(test2.get(i),c);
+            }
+            assertTrue(test2.remove(c));
+        }
+        assertEquals(0,test1.size());
+    }
+}
