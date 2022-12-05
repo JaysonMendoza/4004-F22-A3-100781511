91267800cb9a6192531f585cfad828f6f3edd628
Jayson Mendoza <jayson.k.mendoza@gmail.com>
2022-12-04 21:03:17 -0500

    Code I1: testDeckBuildingHasAllCards has been implemented

--- src/main/java/ca/jkmconsulting/crazyEightsCountdown/Deck.java
+++ src/main/java/ca/jkmconsulting/crazyEightsCountdown/Deck.java
@@ -1,22 +1,54 @@
 package ca.jkmconsulting.crazyEightsCountdown;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Stack;
 
 public class Deck {
-    private ArrayList<Card> drawDeck;
-    private ArrayList<Card> cardsIssued;
-    private ArrayList<Card> discardPile;
+    final private Stack<Card> drawDeck;
+    final private ArrayList<Card> cardsIssued;
+    final private ArrayList<Card> discardPile;
 
     public Deck(ArrayList<Card> fixedOrder) {
-
+        drawDeck = new Stack<>();
+        cardsIssued = new ArrayList<>();
+        discardPile = new ArrayList<>();
+        buildDeck(fixedOrder);
     }
 
     public Deck() {
-
+        this(null);
     }
 
     private void buildDeck(ArrayList<Card> cardOrder) {
-
+        drawDeck.clear();
+        discardPile.clear();
+        ArrayList<Card> allCards = new ArrayList<>(Arrays.asList(Card.values()));
+        Collections.shuffle(allCards);
+//        ArrayList<Card> order = cardOrder;
+        //Add all cards to draw deck that are not included in stacked deck portion
+        if(cardOrder!=null) {
+            for(Card c : allCards) {
+                if(!cardOrder.contains(c)) {
+                    drawDeck.push(c);
+                }
+            }
+            //Add stacked cards in proper order on top of shuffled deck (We draw from the end of the array)
+            for(int i=cardOrder.size()-1;i>=0;--i) {
+                drawDeck.push(cardOrder.get(i));
+            }
+        }
+        else if(!cardsIssued.isEmpty()) { //If a reshuffle, then rebuild deck with the exclusion of issued cards
+            for(Card c : allCards) {
+                if(!cardsIssued.contains(c)) {
+                    drawDeck.push(c);
+                }
+            }
+        }
+        else {
+            drawDeck.addAll(allCards);
+        }
     }
 
     public int getNumCardsInDeck() {
@@ -24,6 +56,11 @@ public class Deck {
     }
 
     public Card drawCard() {
-        return null;
+        if(drawDeck.isEmpty()) {
+            buildDeck(null);
+        }
+        Card c = drawDeck.pop();
+        cardsIssued.add(c);
+        return c;
     }
 }
